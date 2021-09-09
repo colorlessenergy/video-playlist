d0fbe52d60526ca6bafebb9ca9ec21f0823063cc
colorlessenergy <brianmunozdev@gmail.com>
2021-09-09 07:47:58 -0700

    add HTML structure to home page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,30 @@ export default function Home() {
         <meta name="description" content="create video playlist" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+        <form>
+            <label htmlFor="videoLink">
+                add video link
+            </label>
+            <input
+                type="text"
+                id="videoLink" />
+            <button>
+                +
+            </button>
+        </form>
+
+        <div>
+            <div>
+                <div>
+                    x
+                </div>
+                <div>
+
+                </div>
+                <p>this is a title</p>
+            </div>
+        </div>
     </div>
   );
 }
