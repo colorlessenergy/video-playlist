f159be9749fccf91bb0bc0a565a4de896f92cb03
colorlessenergy <brianmunozdev@gmail.com>
2021-09-11 17:33:56 -0700

    add functionality to disable iframes

--- shared/components/EmbedVideo/EmbedVideo.js
+++ shared/components/EmbedVideo/EmbedVideo.js
@@ -1,13 +1,18 @@
-const EmbedVideo = ({ link, website }) => {
+const EmbedVideo = ({ link, website, disabled }) => {
     return (
-        <iframe
-            width="100%"
-            height="200"
-            src={ link }
-            title={ `${ website } video player` }
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen></iframe>
+        <div className="position-relative">
+            <iframe
+                width="100%"
+                height="200"
+                src={ link }
+                title={ `${ website } video player` }
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen></iframe>
+                { disabled ? (
+                    <div className="disabled"></div>
+                ) : (null) }
+        </div>
     );
 }
 
