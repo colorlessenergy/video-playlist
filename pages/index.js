8040efc358e5e488a4d80e8ace735e153612aec5
colorlessenergy <brianmunozdev@gmail.com>
2021-09-11 19:44:41 -0700

    add proper spacing

--- pages/index.js
+++ pages/index.js
@@ -81,104 +81,104 @@ export default function Home() {
     let [ isModalOpen, setIsModalOpen ] = useState(false);
 
     return (
-    <div>
-      <Head>
-        <title>video playlist</title>
-        <meta name="description" content="create video playlist" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <div>
+            <Head>
+                <title>video playlist</title>
+                <meta name="description" content="create video playlist" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <div className="container">
-        <form
-            onSubmit={ handleSubmit }
-            className="flex align-items-end">
-            <div className="flex flex-direction-column align-items-start add-video-link-input-container">
-                <label htmlFor="videoLink">
-                    add video link
-                </label>
-                <input
-                    className="add-video-link-input"
-                    type="text"
-                    id="videoLink"
-                    onChange={ handleVideoLinkInputChange }
-                    value={ videoLink }
-                    placeholder="video link" />
-            </div>
-            <button className="add-video-link-button">
-                +
-            </button>
-        </form>
-
-        <div className="mt-2 flex flex-wrap justify-content-between">
-            { videos.map(video => {
-                return (
-                    <div
-                        key={ video.ID }
-                        className="mr-3 mb-1 video-embed-container">
-                        <div className="text-right">
-                            <button
-                                onClick={ () => handleRemoveVideo(video.ID) }
-                                className="button-red">
-                                x
-                            </button>
-                        </div>
-                        <EmbedVideo
-                            link={ video.link }
-                            website={ video.website }
-                            disabled={ true }
-                            onClick={ () => handleVideoClick(video) } />
+            <div className="container">
+                <form
+                    onSubmit={ handleSubmit }
+                    className="flex align-items-end">
+                    <div className="flex flex-direction-column align-items-start add-video-link-input-container">
+                        <label htmlFor="videoLink">
+                            add video link
+                        </label>
+                        <input
+                            className="add-video-link-input"
+                            type="text"
+                            id="videoLink"
+                            onChange={ handleVideoLinkInputChange }
+                            value={ videoLink }
+                            placeholder="video link" />
                     </div>
-                );
-            }) }
-        </div>
-      </div>
-        { isModalOpen ? (
-            <Modal isOpen={ isModalOpen }>
-                <div className="flex justify-content-between align-items-center mb-1">
-                    { clickedVideo.website === 'YouTube' ? (
-                        <Image
-                            src="/youtube.svg"
-                            alt="YouTube logo"
-                            width={ 25 }
-                            height={ 25 } />
-                    ) : (
-                        <Image
-                            src="/vimeo.svg"
-                            alt="Vimeo logo"
-                            width={ 25 }
-                            height={ 25 } />
-                    ) }
-                    <button
-                        onClick={ closeModal }
-                        className="button-red circle">
-                        x
+                    <button className="add-video-link-button">
+                        +
                     </button>
-                </div>
-                <EmbedVideo
-                    link={ clickedVideo.link }
-                    website={ clickedVideo.website }
-                    disabled={ false }
-                    height={ 384 } />
-                <div className={`flex ${ videos.length >= 3 ? ('overflow-x-scroll') : ('') }`}>
-                    { videos.map(video => {
-                        let isPlaying = false;
-                        if (video.ID === clickedVideo.ID) {
-                            isPlaying = true;
-                        }
+                </form>
 
+                <div className="mt-2 flex flex-wrap justify-content-between">
+                    { videos.map(video => {
                         return (
-                            <EmbedVideo
-                                link={ video.link }
-                                website={ video.website }
-                                disabled={ true }
-                                width={ 400 }
-                                isPlaying={ isPlaying }
-                                onClick={ () => setClickedVideo(video) } />
+                            <div
+                                key={ video.ID }
+                                className="mr-3 mb-1 video-embed-container">
+                                <div className="text-right">
+                                    <button
+                                        onClick={ () => handleRemoveVideo(video.ID) }
+                                        className="button-red">
+                                        x
+                                    </button>
+                                </div>
+                                <EmbedVideo
+                                    link={ video.link }
+                                    website={ video.website }
+                                    disabled={ true }
+                                    onClick={ () => handleVideoClick(video) } />
+                            </div>
                         );
-                    })  }
+                    }) }
                 </div>
-            </Modal>
-        ) : (null) }
-    </div>
-  );
+            </div>
+            { isModalOpen ? (
+                <Modal isOpen={ isModalOpen }>
+                    <div className="flex justify-content-between align-items-center mb-1">
+                        { clickedVideo.website === 'YouTube' ? (
+                            <Image
+                                src="/youtube.svg"
+                                alt="YouTube logo"
+                                width={ 25 }
+                                height={ 25 } />
+                        ) : (
+                            <Image
+                                src="/vimeo.svg"
+                                alt="Vimeo logo"
+                                width={ 25 }
+                                height={ 25 } />
+                        ) }
+                        <button
+                            onClick={ closeModal }
+                            className="button-red circle">
+                            x
+                        </button>
+                    </div>
+                    <EmbedVideo
+                        link={ clickedVideo.link }
+                        website={ clickedVideo.website }
+                        disabled={ false }
+                        height={ 384 } />
+                    <div className={`flex ${ videos.length >= 3 ? ('overflow-x-scroll') : ('') }`}>
+                        { videos.map(video => {
+                            let isPlaying = false;
+                            if (video.ID === clickedVideo.ID) {
+                                isPlaying = true;
+                            }
+
+                            return (
+                                <EmbedVideo
+                                    link={ video.link }
+                                    website={ video.website }
+                                    disabled={ true }
+                                    width={ 400 }
+                                    isPlaying={ isPlaying }
+                                    onClick={ () => setClickedVideo(video) } />
+                            );
+                        })  }
+                    </div>
+                </Modal>
+            ) : (null) }
+        </div>
+    );
 }
